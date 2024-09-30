4e3f96521f88a0ed207dd56ab276b498c3d4969a
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2024-09-30 16:40:34 -0300

    shadcn no front
    shadcn no front

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,11 +5,15 @@ import dotenv from "dotenv"
 import { validateCPF } from "../utils/validarCPF.js"
 import { errorHandler } from "../utils/error.js"
 
-// Corrigir __dirname para módulos ES
 dotenv.config();
 
+//Cadastro
 export const cadastro = async(req, res, next) => {
     const { username, email, password, confirmPassword, cpf, gender } = req.body
+    //Verificar se todos os campos foram preenchidos
+    if (!username || !email || !password || !confirmPassword || !cpf || !gender) {
+        return next(errorHandler(400, 'Por favor, preencha todos os campos'));
+    }
     //Limpar os CPF
     const cleanedCPF = cpf.replace(/\D/g, '');
     //Validações Iniciais
@@ -22,13 +26,11 @@ export const cadastro = async(req, res, next) => {
             message: 'CPF inválido',
         })
     }
-    
     //Verifica se o email já está em uso
     let validUser = await User.findOne({ where: { email } });
     if(validUser){
         return next(errorHandler(400, 'Este email ja existe'))
     }
-
     //Verifica se o CPF já está em uso
     let validUserCPF = await User.findOne({ where: { cpf: cleanedCPF } });
     if(validUserCPF){
@@ -40,11 +42,10 @@ export const cadastro = async(req, res, next) => {
     }
     //Criptografar a senha
     const hashedPassword = bcryptjs.hashSync(password, 10)
-
     //Define a imagem de perfil com base no genero
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
-
+    //Criar o usuário
     const newUser = new User({
         username,
         email,
@@ -53,7 +54,6 @@ export const cadastro = async(req, res, next) => {
         gender,
         profilePicture: gender === 'Masculino' ? boyProfilePic : girlProfilePic
     })
-
     try{
         //Gerar Token JWT
         const token = jwt.sign({
@@ -76,8 +76,37 @@ export const cadastro = async(req, res, next) => {
     }
 }
 
-export const login = (req, res) => {
-
+//Login
+export const login = (req, res, next) => {
+    const { email, password } = req.body
+    if(!email || !password){
+        return next(errorHandler(400, 'Preencha todos os campos'))
+    }
+    try{
+        const user = User.findOne({ where: { email } })
+        if(!user){
+            return next(errorHandler(400, 'Email ou senha inválido'))
+        }
+        const match = bcryptjs.compareSync(password, user.password)
+        if(!match){
+            return next(errorHandler(400, 'Email ou senha inválido'))
+        }
+        const token = jwt.sign({
+            id: user._id,
+        }, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+        })
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token,
+            profilePicture: user.profilePicture,
+        })
+    }catch(error){
+        console.log("Erro ao logar", error)
+        return next(errorHandler(500, 'Internal Server Error'))
+    }
 }
 
 export const logout = (req, res) => {
