4125668c918457b7a4a24c8f19bee298dd62f358
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2024-10-01 18:25:11 -0300

    rota de usuários no back + front página de login / registro
    Páginas ainda sem integração com o backend, esqueci a senha ainda não existe

--- backend/routes/UseRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getUsers } from '../controllers/user.controller.js'; // ajuste o caminho conforme necessário
-
-const router = express.Router();
-
-router.get('/users', getUsers);
-
-export default router;
