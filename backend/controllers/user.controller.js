d8faaee9aea86365d12aed4a46aeac1001f76d19
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2024-10-01 16:34:05 -0300

    rota para buscar usuários
    rota para buscar usuários

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,6 @@
-import User from '../models/user.js'; // ajuste o caminho conforme necessário
+import models from '../models/index.js';
+import { Op } from 'sequelize';
+const { User } = models;
 
 export const getUsers = async (req, res) => {
     try {
@@ -8,3 +10,24 @@ export const getUsers = async (req, res) => {
         res.status(500).send('Erro ao buscar usuários: ' + err);
     }
 };
+
+export const getUserForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user.id; // ID do usuário logado
+
+        // Busca todos os usuários exceto o logado
+        const allUserExceptLoggedInUser = await User.findAll({
+            where: {
+                id: {
+                    [Op.not]: loggedInUserId // Excluir o usuário logado
+                }
+            },
+            attributes: { exclude: ['password'] } // Excluir a senha do resultado
+        });
+
+        res.status(200).json(allUserExceptLoggedInUser); // Enviar o resultado como JSON
+    } catch (error) {
+        next(error);
+    }
+};
+
