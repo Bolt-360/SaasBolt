19cf451cee8aad1c7797ed0b540fa0d2ded16209
Michael Andryeer <andryeerm@gmail.com>
2024-10-02 17:04:46 -0300

    commit inicial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home'
 import AuthPage from './pages/auth'
+import { SignInCard } from './features/auth/components/sign-in-card'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />
+        <Route path='/sign-in' element={<SignInCard />} />
       </Routes>
     </div>
   )
--- frontend/src/features/auth/components/sign-up-card.jsx
+++ frontend/src/features/auth/components/sign-up-card.jsx
@@ -1,16 +1,11 @@
 import { useState, useEffect } from "react";
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { SignInFlow } from "../types";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { CardTitle } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
 import { Progress } from "@/components/ui/progress";
-
-/**
- * @typedef {Object} SignInCardProps
- * @property {function(SignInFlow): void} setState
- */
+import { useNavigate } from "react-router-dom"; // Importando useNavigate
 
 /**
  * @param {string} password
@@ -25,20 +20,19 @@ const passwordStrength = (password) => {
   return strength;
 };
 
-/**
- * @param {SignInCardProps} props
- * @returns {JSX.Element}
- */
-export const SignUpCard = ({setState}) => {
+export const SignUpCard = ({ setState }) => {
   const { toast } = useToast();
+  const navigate = useNavigate(); // Criando uma instância de navigate
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [pending, setPending] = useState(false);
   const [passwordStrengthValue, setPasswordStrengthValue] = useState(0);
-  const [cpf, setCpf] = useState("")
+  const [cpf, setCpf] = useState("");
+  const [gender, setGender] = useState(""); 
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
 
   useEffect(() => {
     setPasswordStrengthValue(passwordStrength(password));
@@ -46,166 +40,258 @@ export const SignUpCard = ({setState}) => {
 
   const onPasswordSignUp = async (e) => {
     e.preventDefault();
-    
-    if(password !== confirmPassword){
+
+    // Verifica se as senhas são iguais
+    if (password !== confirmPassword) {
       toast({
         title: "Erro ao cadastrar",
-        description: "As senhas precisam ser iguais!",
+        description: "As senhas não coincidem. Por favor, tente novamente.",
         variant: "destructive",
       });
-      return;
+      return; // Impede que o formulário continue se as senhas forem diferentes
     }
-    if (passwordStrengthValue < 75) {
+
+    // Verifica a força da senha
+    if (passwordStrength < 75) {
       toast({
         title: "Senha fraca",
         description: "Por favor, use uma senha mais forte antes de cadastrar.",
         variant: "destructive",
       });
-      return;
+      return; // Impede que o formulário continue se a senha for fraca
     }
-    setPending(true);
-    console.log('Signup attempt:', { name, email, password, cpf });
-    setTimeout(() => {
-      setPending(false);
-      toast({
-        title: "Cadastro simulado",
-        description: "Dados de cadastro registrados no console.",
-        variant: "default",
-      });
-    }, 1000);
-  }
 
-  /**
-   * @param {React.ChangeEvent<HTMLInputElement>} e
-   */
-  const handleCPFChange = (e) => {
-    setCpf(e.target.value);
+    setPending(true);
+
+    try {
+      const response = await fetch("http://localhost:2345/api/auth/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: name,
+          email,
+          password,
+          confirmPassword,
+          cpf,
+          gender,
+        }),
+      });
+
+      const data = await response.json(); // Extraímos a resposta da API
+
+      if (!response.ok) {
+        if (data.message && data.message.includes("CPF já registrado")) {
+          toast({
+            title: "Erro ao cadastrar",
+            description: "Este CPF já está em uso. Tente outro.",
+            variant: "destructive",
+          });
+        } else {
+          toast({
+            title: "Erro ao cadastrar",
+            description: data.message || "Erro desconhecido.",
+            variant: "destructive",
+          });
+        }
+      } else {
+        toast({
+          title: "Cadastro realizado com sucesso!",
+          description: "Você pode fazer login agora.",
+          variant: "default",
+        });
+        setRegistrationSuccess(true);
+
+        // Limpa os campos após sucesso no cadastro
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+        setCpf("");
+        setGender("");
+
+        setTimeout(() => {
+          navigate("/sign-in");
+        }, 2000);
+      }
+    } catch (error) {
+      toast({
+        title: "Erro na requisição",
+        description: "Não foi possível realizar o cadastro. Tente novamente.",
+        variant: "destructive",
+      });
+    } finally {
+      setPending(false);
+    }
   };
 
-  return(
-      <>
-          <div className="bg-card rounded-2xl shadow-lg ring-1 ring-border">
-              <CardTitle className="text-center text-1xl font-bold text-primary pt-4">Crie sua conta aqui</CardTitle>
-          <div className="px-6 py-8 sm:px-10">
-            <form className="space-y-6" onSubmit={onPasswordSignUp}>
-              <div>
-                <Label htmlFor="cpf" className="block text-sm font-medium text-muted-foreground">
-                  CPF
-                </Label>
-                <div className="mt-1">
-                  <Input
-                    id="cpf"
-                    name="cpf"
-                    type="text"
-                    autoComplete="cpf"
-                    value={cpf}
-                    onChange={handleCPFChange}
-                    required
-                    placeholder="000.000.000-00"
-                    className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
-                  />
-                </div>
+  // Função para formatar o CPF
+  const formatCPF = (value) => {
+    value = value.replace(/\D/g, ""); 
+    value = value.replace(/(\d{3})(\d)/, "$1.$2"); 
+    value = value.replace(/(\d{3})(\d)/, "$1.$2"); 
+    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2"); 
+    return value;
+  };
+
+  const handleCPFChange = (e) => {
+    const formattedCpf = formatCPF(e.target.value);
+    setCpf(formattedCpf); // Atualiza o estado com o CPF formatado
+  };
+
+  return (
+    <>
+      <div className="bg-card rounded-2xl shadow-lg ring-1 ring-border">
+        <CardTitle className="text-center text-1xl font-bold text-primary pt-4">
+          Crie sua conta aqui
+        </CardTitle>
+        <div className="px-6 py-8 sm:px-10">
+          <form className="space-y-6" onSubmit={onPasswordSignUp}>
+            <div>
+              <Label
+                htmlFor="cpf"
+                className="block text-sm font-medium text-muted-foreground"
+              >
+                CPF
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="cpf"
+                  name="cpf"
+                  type="text"
+                  autoComplete="cpf"
+                  value={cpf}
+                  onChange={handleCPFChange}
+                  required
+                  placeholder="000.000.000-00"
+                  className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
+                />
               </div>
-              <div>
-                <Label htmlFor="name" className="block text-sm font-medium text-muted-foreground">
-                  Nome
-                </Label>
-                <div className="mt-1">
-                  <Input
-                    id="name"
-                    name="name"
-                    type="text"
-                    autoComplete="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                    placeholder="Nome completo"
-                    className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
-                  />
-                </div>
-              </div>
-              <div>
-                <Label htmlFor="email" className="block text-sm font-medium text-muted-foreground">
-                  Email 
-                </Label>
-                <div className="mt-1">
-                  <Input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    placeholder="email@bolt360.com.br"
-                    className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <Label htmlFor="password" className="block text-sm font-medium text-muted-foreground">
-                    Senha
-                  </Label>
-                </div>
-                <div className="mt-1">
-                  <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="new-password"
-                    required
-                    placeholder="********"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <Label htmlFor="password-confirm" className="block text-sm font-medium text-muted-foreground">
-                    Confirme a senha
-                  </Label>
-                </div>
-                <div className="mt-1">
-                  <Input
-                    id="password-confirm"
-                    name="password-confirm"
-                    type="password"
-                    autoComplete="new-password"
-                    required
-                    placeholder="********"
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    value={confirmPassword}
-                    className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
-                  />
-                </div>
-                <Progress value={passwordStrengthValue} className="mt-2" />
-                  <p className="text-sm text-muted-foreground mt-1">
-                    Força da senha: {passwordStrengthValue < 25 ? "Fraca" : passwordStrengthValue < 50 ? "Média" : passwordStrengthValue < 75 ? "Boa" : "Forte"}
-                  </p>
-              </div>
-              <div>
-                <Button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-primary-foreground shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
-                  disabled={pending}
-                >
-                  {pending ? "Cadastrando..." : "Cadastrar"}
-                </Button>
-              </div>
-            </form>
-          </div>
-          </div>
-          <div className="flex items-center justify-center">
-            <div className="text-sm">
-              Já tem uma conta?{" "}
-              <span onClick={() => setState('sign-in')} className="font-medium text-primary hover:text-primary/90 hover:cursor-pointer hover:underline">
-                Faça login
-              </span>
             </div>
-          </div>
-      </>
-  )
-}
+            <div>
+              <Label
+                htmlFor="name"
+                className="block text-sm font-medium text-muted-foreground"
+              >
+                Nome
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                  placeholder="Nome completo"
+                  className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
+                />
+              </div>
+            </div>
+            <div>
+              <Label
+                htmlFor="email"
+                className="block text-sm font-medium text-muted-foreground"
+              >
+                Email
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="email@bolt360.com.br"
+                  className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
+                />
+              </div>
+            </div>
+            <div>
+              <Label
+                htmlFor="gender"
+                className="block text-sm font-medium text-muted-foreground"
+              >
+                Gênero
+              </Label>
+              <div className="mt-1">
+                <select
+                  id="gender"
+                  name="gender"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  required
+                  className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
+                >
+                  <option value="">Selecione o gênero</option>
+                  <option value="Masculino">Masculino</option>
+                  <option value="Feminino">Feminino</option>
+                </select>
+              </div>
+            </div>
+            <div>
+              <Label
+                htmlFor="password"
+                className="block text-sm font-medium text-muted-foreground"
+              >
+                Senha
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="new-password"
+                  required
+                  placeholder="********"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
+                />
+              </div>
+            </div>
+            <div>
+              <Label
+                htmlFor="password-confirm"
+                className="block text-sm font-medium text-muted-foreground"
+              >
+                Confirme a senha
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="password-confirm"
+                  name="confirmPassword"
+                  type="password"
+                  autoComplete="new-password"
+                  required
+                  placeholder="********"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  value={confirmPassword}
+                  className="w-full rounded-md border-input bg-background px-3 py-2 text-foreground shadow-sm focus:border-primary focus:ring-primary"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="mt-1">
+                <Progress value={passwordStrengthValue} />
+              </div>
+            </div>
+            <div>
+              <Button
+                className="w-full rounded-md bg-primary py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+                type="submit"
+                disabled={pending}
+              >
+                {pending ? "Cadastrando..." : "Cadastrar"}
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
