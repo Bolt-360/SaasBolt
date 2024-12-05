184bb0dbe87738366391675684445a117b287227
Lucas Zerino <lucaszerino@gmail.com>
2024-12-05 11:06:55 -0300

    sga controller e routes
    sga controller e routes

---
+++ backend/routes/sga.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { syncSgaCredentials } from '../controllers/sgaController.js';
+import { createSgaUser, syncSgaCredentials } from '../controllers/sgaController.js';
+import { isAuthenticate } from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.post('/create-user', isAuthenticate, createSgaUser);
+router.post('/sync-credentials', isAuthenticate, syncSgaCredentials);
+
+export default router;
